d5414f257230f314b22d6ad425aadd79dc9ad2f6
Rishi2600 <rishiraj6177@gmail.com>
2025-04-07 23:34:04 +0530

    added peer connection

--- talkIN/main.js
+++ talkIN/main.js
@@ -1,9 +1,24 @@
 let localStream;
 let remoteStream;
+let peerConnection;
 
 async function init() {
     localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
     document.getElementById("user-1").srcObject = localStream;
+
+    createOffer();
+}
+
+async function createOffer() {
+    peerConnection = new RTCPeerConnection();
+
+    remoteStream = new MediaStream();
+    document.getElementById("user-2").srcObject = remoteStream;
+
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer);
+
+    console.log(offer);
 }
 
 init();
