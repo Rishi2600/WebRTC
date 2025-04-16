8c2071e2af153c100cb78fe4cf8ed8cf7e87b3cf
Rishi2600 <rishiraj6177@gmail.com>
2025-04-17 01:01:21 +0530

    enhanced video quality

--- weTALK/js/room_rtc.js
+++ weTALK/js/room_rtc.js
@@ -32,7 +32,10 @@ async function joinRoomInit() {
 }
 
 async function joinStream() {
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks({}, {encoderConfig: {
+        width: {min: 640, ideal: 1920, max: 1920},
+        height: {min: 480, ideal: 1080, max: 1080}
+    }})
 
     let player = `<div class="video_container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}></div>
