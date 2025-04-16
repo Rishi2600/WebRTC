7702c2e4305d8b22b0506336f0bfb920c3bac6a1
Rishi2600 <rishiraj6177@gmail.com>
2025-04-16 23:06:53 +0530

    handled published tracks from user

--- weTALK/js/room_rtc.js
+++ weTALK/js/room_rtc.js
@@ -25,6 +25,8 @@ async function joinRoomInit() {
     client = AgoraRTC.createClient({mode: "rtc", codec: "vp8"})
     await client.join(APP_ID, roomId, token, uid)
 
+    client.on("user-published", handleUserPublished)
+
     joinStream()
 }
 
@@ -38,6 +40,32 @@ async function joinStream() {
     document.getElementById("streams_container").insertAdjacentHTML("beforeend", player)
 
     localTracks[1].play(`user-${uid}`)
+
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+async function handleUserPublished(user, mediaType) {
+    remoteUsers[user.uid] = user;
+
+    await client.subscribe(user, mediaType)
+
+    let player = document.getElementById(`user-container-${user.uid}`)
+    if(player===null) {
+
+        player = `<div class="video_container" id="user-container-${user.uid}">
+                    <div class="video-player" id="user-${user.uid}></div>
+                  </div>`
+
+    document.getElementById("stream_container").insertAdjacentHTML("beforeend", player)
+
+    }
+
+    if(mediaType === "video") {
+        user.videoTrack.play(`user-${user.uid}`)
+    }
+    if(mediaType==="audio") {
+        user.audioTrack.play()
+    }
 }
 
 joinRoomInit();
