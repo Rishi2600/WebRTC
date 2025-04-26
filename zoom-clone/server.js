15d1a5ef1494471e8a9110942e355a75e79d2d64
Rishi2600 <rishiraj6177@gmail.com>
2025-04-26 23:47:38 +0530

    created a very basic server for signaling

---
+++ zoom-clone/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+
+const port = 3000
+
+server.listen(port, () => {
+    console.log(`server is listening on port: ${port}`)
+})
