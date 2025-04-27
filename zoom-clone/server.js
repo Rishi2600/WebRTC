f3bd2a42a013e48a63d5c34548e4398579073d2a
Rishi2600 <rishiraj6177@gmail.com>
2025-04-27 23:49:08 +0530

    added a view, made some changes on the server

--- zoom-clone/server.js
+++ zoom-clone/server.js
@@ -3,12 +3,13 @@ const app = express()
 
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
+const { v4: uuidV4 } = require("uuid")
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-
+    res.redirect(`/${uuidV4()}`)
 })
 
 app.get("/:room", (req, res) => {
