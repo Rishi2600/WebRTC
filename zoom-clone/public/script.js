bae3fc5c6a77bae3c6df60913d5ea52a52c96e3a
Rishi2600 <rishiraj6177@gmail.com>
2025-04-29 15:18:05 +0530

    added video stream

--- zoom-clone/public/script.js
+++ zoom-clone/public/script.js
@@ -1,8 +1,19 @@
 const socket = io("/")
 
+const videoGrid = document.getElementById("video-grid") 
 const myPeer = new Peer(undefined, {
     host: "/",
     port: "3001"
+})
+
+const myVideo = document.addEventListener(("video"))
+myVideo.mute = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then( stream => {
+    addVideoStream(myVideo, stream)
 })
 
 myPeer.on("open", id => {
@@ -13,3 +24,12 @@ myPeer.on("open", id => {
 socket.on("user-connected", userId => {
     console.log("user-connected" + userId)
 })
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream
+
+    video.addEventListener("loadedMetaData", () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
